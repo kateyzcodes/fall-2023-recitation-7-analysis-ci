1a981d9a4c6f57535b1301201da64ab3f379c751
kateyzcodes <103164368+kateyzcodes@users.noreply.github.com>
2023-10-27 15:56:23 -0400

    Added tool formatter and associated workflow (#2)
    * made some changes to main.yml for formatter workflow
    
    * fixed syntax error in main.yml for formatter workflow
    
    * fixed indentation error in main.yml
    
    * fixed minor error that occurred in fib.py

--- app/fib.py
+++ app/fib.py
@@ -8,7 +8,7 @@ def fibonacci(position):
   if(position < 0):
     raise ValueError("Invalid input")
   if position == 0:
-    return 0
+        return 0
   if position == 1:
-    return 1
+        return 1
   return fibonacci(position - 1) + fibonacci(position - 2)
